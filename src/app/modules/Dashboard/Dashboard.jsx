ed6fef93e7e46e2bf1d64ed91f3847f72bb31515
Luis Miguel Cabezas <luismiguel.cabezas@gmail.com>
2024-10-07 23:26:43 +0200

    Datos del token

--- src/app/modules/Dashboard/Dashboard.jsx
+++ src/app/modules/Dashboard/Dashboard.jsx
@@ -28,15 +28,21 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { jwtDecode } from "jwt-decode"
 
 export const Dashboard = () => {
+
+    const token = localStorage.getItem('token')
+    const decodedToken = jwtDecode(token)
+    console.log(decodedToken)
+
     return (
         <>
             <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
                 <Card x-chunk="dashboard-01-chunk-0">
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
-                            Total Revenue
+                            Datos del Token
                         </CardTitle>
                         <DollarSign className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
